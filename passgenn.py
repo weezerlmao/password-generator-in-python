6677385491c15e38069294c79970faec35f754cc
weezerlmao <noiarentbretosh@gmail.com>
2023-07-06 08:09:21 +0300

    Restructured the code

--- passgenn.py
+++ passgenn.py
@@ -1,27 +1,41 @@
+
 import random
 
 x = "QWERTYUIOPASDFGHJKLZXCVBNM"
 y = "qwertyuiopasdfghjklzxcvbnm"
 z = "123456789@£_&$§∆✓%?!"
 
-password_letters = x + y + z + y + x + z
+password_letters = x + y + z + x + z + y
+words_yes = ["yes", "ya", "yup", "yip", "y", "yah", "yws", "ye", "yas", "yass", "yee", "yeo", "yah", "sure", "ok", "okay", "sur", "surw"]
 
-choice = input("Need a password?: ")
+choice = input("Need a pass?: ")
 
+#hl stands for highlight
 hll = '\033[1;0m'
 hl1 = '\033[1;43m'
 hl2 = '\033[1;44m'
 hl3 = '\033[1;41m'
 hl4 = '\033[1;42m'
 
-hl_col = [hl1, hl2, hl3]
+hl_col = [hl1, hl2, hl3, hl4]
 hl_colour = random.choice(hl_col)
 
-if choice == "yes" or choice == "yea" or choice == "yws" or choice == "ya" or choice == "ye" or choice == "yup" or choice == "yawh" or choice == "y":
-    password = "".join(random.choices(password_letters, k  = random.randint(9, 19)))
-    password_high = hl_colour  + password + hll
-    print(f"{password_high} is the generated password, keep it safe buddy")
-    print("")
-    print(password_high)
-else:
-    print("Cancelled.")
+def gen_pass():
+     global choice
+     if choice in words_yes:
+         password = "".join(random.choices(password_letters, k  = random.randint(9, 19)))
+         global hl_colour
+         password_high = hl_colour + password + hll
+         print(f"{password_high} is the generated password, keep it safe buddy")
+         print("")
+         print(password_high)
+         again = input("Another?: ")
+         if again in words_yes:
+             hl_colour = random.choice(hl_col)
+             gen_pass()
+         else:
+             print("Cancelled.")
+     else:
+         print("Cancelled.")
+         
+gen_pass()
